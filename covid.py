fc05143ec27165135c577dea723f279f6264a85f
Tee55 <32050764+Tee55@users.noreply.github.com>
2021-06-26 16:42:06 +0800

    Add files via upload

--- covid.py
+++ covid.py
@@ -1,6 +1,4 @@
 import numpy as np
-from PIL import Image
-import glob
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import matplotlib.pyplot as plt
 from tensorflow.keras.models import Sequential
@@ -8,44 +6,34 @@ from tensorflow.keras.layers import Dense,Conv2D,Flatten,MaxPooling2D
 from tensorflow.keras.callbacks import EarlyStopping,ReduceLROnPlateau
 from sklearn.utils.class_weight import compute_class_weight
 import pandas as pd
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import confusion_matrix
 import seaborn as sns
-from tensorflow.keras.applications.vgg19 import VGG19
-from tensorflow.keras.models import Model
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.vgg19 import preprocess_input
 
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import RepeatedStratifiedKFold
-from sklearn.pipeline import Pipeline
-from sklearn.decomposition import PCA
-from sklearn.linear_model import LogisticRegression
-from sklearn.cluster import KMeans
-
-import tensorflow as tf
-gpus = tf.config.experimental.list_physical_devices('GPU')
-tf.config.experimental.set_memory_growth(gpus[0], True)
-
-#Define Directories for train, test & Validation Set
+#Define Directories for train, test
 train_path = 'dataset/train'
 test_path = 'dataset/test'
-valid_path = 'dataset/val'
 
 batch_size = 10
+
+'''
+img_height = 299
+img_width = 299
+'''
 
 img_height = 224
 img_width = 224
 
-test_data_gen = ImageDataGenerator(rescale = 1./255)
-image_gen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
+def main():
 
-def train():
+    image_gen = ImageDataGenerator(rescale = 1./255, shear_range = 0.2, zoom_range = 0.2, horizontal_flip = True)
+    test_data_gen = ImageDataGenerator(rescale = 1./255)
 
-    train = image_gen.flow_from_directory(train_path, target_size=(img_height, img_width), color_mode='grayscale', batch_size=batch_size)
-    valid = test_data_gen.flow_from_directory(valid_path, target_size=(img_height, img_width), color_mode='grayscale', batch_size=batch_size)
+    train = image_gen.flow_from_directory(train_path, target_size=(img_height, img_width), class_mode='categorical', color_mode='grayscale',  batch_size=batch_size)
+    test = test_data_gen.flow_from_directory(test_path, target_size=(img_height, img_width), class_mode='categorical', color_mode='grayscale', shuffle=False, batch_size=batch_size)
 
     cnn = create_model()
 
+    #Deafult Learning rate = 0.01
     early = EarlyStopping(monitor='val_loss', mode='min', patience=2)
     learning_rate_reduction = ReduceLROnPlateau(monitor='val_loss', patience=2, verbose=1, factor=0.3, min_lr=0.000001)
 
@@ -54,27 +42,17 @@ def train():
     weights = compute_class_weight('balanced', np.unique(train.classes), train.classes)
     cw = dict(zip( np.unique(train.classes), weights))
 
-    cnn.fit(train, epochs=25, validation_data=valid, class_weight=cw, callbacks=callbacks_list)
-
-    cnn.save_weights("covid.h5")
-
-def test():
-
-    test = test_data_gen.flow_from_directory(test_path, target_size=(img_height, img_width), color_mode='grayscale', batch_size=batch_size, shuffle=False)
-
-    cnn = create_model()
-
-    cnn.load_weights("covid.h5")
+    cnn.fit(train, epochs=25, class_weight=cw, callbacks=callbacks_list)
 
     test_accu = cnn.evaluate(test)
     print('The testing accuracy is :', test_accu[1]*100, '%')
 
-    preds = np.argmax(cnn.predict(test), axis=-1)
+    pred_class = np.argmax(cnn.predict(test), axis=-1)
 
-    print(preds)
-
-    cm = pd.DataFrame(data=confusion_matrix(test.classes, preds), index=["Actual Normal", "Actual Covid", "Actual Pneumonia"], columns=["Predicted Normal", "Predicted Covid", "Predicted Pneumonia"])
+    cm = pd.DataFrame(data=confusion_matrix(test.classes, pred_class),index=["Actual Normal", "Actual Covid", "Actual Pneumonia"],
+    columns=["Predicted Normal", "Predicted Covid", "Predicted Penumonia"])
     sns.heatmap(cm, annot=True, fmt="d")
+    print(cm)
 
     plt.show()
 
@@ -93,13 +71,12 @@ def create_model():
     cnn.add(Conv2D(64, (3, 3), activation="relu", input_shape=(img_width, img_height, 1)))
     cnn.add(MaxPooling2D(pool_size = (2, 2)))
     cnn.add(Flatten())
-    cnn.add(Dense(units = 128, activation = 'relu'))
-    cnn.add(Dense(units = 64, activation = 'relu'))
-    cnn.add(Dense(units = 3, activation = 'softmax'))
+    cnn.add(Dense(activation = 'relu', units = 128))
+    cnn.add(Dense(activation = 'relu', units = 64))
+    cnn.add(Dense(activation = 'softmax', units = 3))
     cnn.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 
     return cnn
 
 if __name__ == '__main__':
-    train()
-    test()
+    main()
